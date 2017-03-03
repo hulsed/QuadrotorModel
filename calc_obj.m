6548cfcc04a8898befa27dcbcd2dc99d36cfb49f
hulsed <hulsed@oregonstate.edu>
2017-03-03 11:49:37 -0800

    Re-added climb objective (which is removed in the MDO project, but we want it in the research project.)

--- calc_obj.m
+++ calc_obj.m
@@ -5,11 +5,15 @@ function [obj, constraints] = calc_obj(battery, motor, prop, foil, rod, sys)
     write_propfile(prop,foil);
     %calculate hover performance
     [hover] = calc_hover(sys);
+    ClimbVel=10; %velocity requirement for climb: 10 m/s (22 mph)
+    [climb] = calc_climb(sys,ClimbVel);
 
     %Calculation to find out if any of the objective failed
     if isnan(hover.pelec)
         failure=1;
     elseif hover.failure==1
+        failure=1;
+    elseif climb.failure==1
         failure=1;
     end
     % Calculation of Constraints (only possible with performance data) 
@@ -22,8 +26,12 @@ function [obj, constraints] = calc_obj(battery, motor, prop, foil, rod, sys)
     % Calculates objectives
     Objectives.totalCost =sys.cost;
     Objectives.flightTime = battery.Energy /(4*hover.pelec+sys.power); %note: power use is for EACH motor.
-
+    distance= 300; % climb distance in meters--temp, should be specified elsewhere
+    time=distance/ClimbVel;
+    energy=time*4*(climb.pelec+sys.power);
+    Objectives.climbEnergy=time*climb.pelec;
+    
     %Adding Objectives together...
-    obj=Objectives.flightTime-3*(Objectives.totalCost);
+    obj=Objectives.flightTime+(-Objectives.climbEnergy)/5-3*(Objectives.totalCost);
    end
 end
